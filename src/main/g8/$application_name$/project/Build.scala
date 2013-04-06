6bdef586dff16bad2b99abd6d4ccd414a0fdd173
Adam Saunders <saunders@cs.umass.edu>
2013-04-05 22:04:54 -0400

    ()

--- src/main/g8/$application_name$/project/Build.scala
+++ src/main/g8/$application_name$/project/Build.scala
@@ -4,16 +4,23 @@ import play.Project._
 
 object ApplicationBuild extends Build {
   
-  val appName         = "$application_name$"
+  val appFront         = "$application_name$-front"
+  val appCore          = "$application_name$-core"
+  def prjPath = (s:String) => "prj-"+s
+
   val appVersion      = "0.1-SNAPSHOT"
-  
-  val appDependencies = Seq(
-    // Add your project dependencies here,
+
+  val coreDependencies = Seq(
     "org.scalaz" %% "scalaz-core" % "7.0.0-M7" withSources(),
     "org.specs2" %% "specs2" % "latest.release" % "test"
   )
 
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-    // Add your own project settings here
+  val core = Project(appCore, prjPath(appCore), appVersion, coreDependencies, mainLang = SCALA).settings(
+
   )
+
+  val front = play.Project(appFront, prjPath(appFront), appVersion, mainLang = SCALA).settings(
+
+  ) dependsOn core
+
 }
