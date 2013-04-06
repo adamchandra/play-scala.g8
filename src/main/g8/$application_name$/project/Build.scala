523629d7a849ef4b8e1e654fa48ce7615325ca8e
Adam Saunders <saunders@cs.umass.edu>
2013-04-05 22:18:44 -0400

    ()

--- src/main/g8/$application_name$/project/Build.scala
+++ src/main/g8/$application_name$/project/Build.scala
@@ -8,6 +8,12 @@ object ApplicationBuild extends Build {
   val appCore          = "$application_name$-core"
   def prjPath = (s:String) => "prj-"+s
 
+  val scalaSettings = Seq(
+    scalaVersion := "2.10.1",
+    scalacOptions := Seq("-Xlint", "-deprecation", "-unchecked", "-Xcheckinit", "-encoding", "utf8"),
+    javacOptions ++= Seq("-Xlint:unchecked", "-encoding", "utf8")
+  )
+
   val appVersion      = "0.1-SNAPSHOT"
 
   val coreDependencies = Seq(
@@ -15,12 +21,25 @@ object ApplicationBuild extends Build {
     "org.specs2" %% "specs2" % "latest.release" % "test"
   )
 
-  val core = Project(appCore, prjPath(appCore), appVersion, coreDependencies, mainLang = SCALA).settings(
-
+  // val core = Project(appCore, prjPath(appCore), applicationVersion=appVersion, dependencies=coreDependencies).settings(
+  val core = Project(
+    id = appCore, 
+    base = prjPath(appCore),
+    aggregate = Nil, // : => Seq[ProjectReference] = Nil, 
+    // dependencies = coreDependencies, // : => Seq[ClasspathDep[ProjectReference]] = Nil,
+    delegates = Nil // : => Seq[ProjectReference] = Nil,
+  ).settings(
+    scalaSettings:_*
   )
 
-  val front = play.Project(appFront, prjPath(appFront), appVersion, mainLang = SCALA).settings(
 
+  val front = play.Project(
+    name = appFront,
+    applicationVersion = appVersion,
+    dependencies = Nil, 
+    path = prjPath(appFront)
+  ) settings (
+    // 
   ) dependsOn core
 
 }
